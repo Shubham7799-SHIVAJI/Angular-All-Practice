8b034855e5b87c51bfbe121068e65cdefe377f99
Shubham Agarwal <agarwal.77.99shubham@gmail.com>
2025-10-08 22:09:13 +0530

    formsarray also

--- src/app/movieform/movieform.ts
+++ src/app/movieform/movieform.ts
@@ -6,6 +6,7 @@ import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { FormGroup } from '@angular/forms';
 import { inject } from '@angular/core';
+import { FormArray } from '@angular/forms';
 
 @Component({
   selector: 'app-movieform',
@@ -20,17 +21,37 @@ export class Movieform {
     name:['',[Validators.required,Validators.pattern('^[a-zA-Z ]*$')]],
 
     // ,  Validators.pattern('^[a-zA-Z ]*$')]
-    date:['',Validators.required]
+    date:['',Validators.required],
+    genres:this.fb.array([this.fb.control('',Validators.required)]),
+    
   });
   }
   get f(){
     return this.movieForm.controls;
   }
+  get genres(): FormArray {
+  return this.movieForm.get('genres') as FormArray;
+}
+
+  // Add a new genre
+  addGenre() {
+    this.genres.push(this.fb.control('', Validators.required));
+  }
+
+  // Remove a genre
+  removeGenre(index: number) {
+    this.genres.removeAt(index);
+  }
+
   onSubmit(){
     if(this.movieForm.valid){
       console.log('Form is submitted',this.movieForm.value);
       this.movieForm.reset();
 
+  //     while (this.genres.length) {
+  //   this.genres.removeAt(0);
+  // }
+      this.genres.clear();
     }
     else{
       alert('please eneter all the details');
